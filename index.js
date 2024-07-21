f96be3e3eeaf6b5d6d59a3e5f52ac85e6119b39f
PRO0FESSOR <siddharth96549@gmail.com>
2024-07-21 14:56:04 +0530

    intergerating socket.io  with express and httpserver

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 
 app.get('/',(req,res)=>{
     const filePath = path.join(__dirname, 'index.html');
     res.sendFile(filePath);
+})
+
+io.on('connection',(socket)=>{
+    console.log("a is connected");
+    socket.on('disconnect',()=>{
+        console.log("a is disconnected");
+    })
 })
 
 server.listen(8080,()=>{
